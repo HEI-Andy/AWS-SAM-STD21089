57eef641ecd4e9f123ca90de5c18a4b6dce2c27e
HARIVOLA Andy <hei.andy.8@gmail.com>
2023-05-23 17:26:31 +0300

    feat: add deployment link to the readme file

---
+++ README.md
@@ -0,0 +1,2 @@
+### Voici le lien du déploiment 
+https://3e1jqjmhol.execute-api.us-east-1.amazonaws.com/Prod/hello/?a=17&&b=25
---
+++ hello_world/app.py
@@ -0,0 +1,12 @@
+import json
+
+def lambda_handler(event, context):
+
+    a = int(event['queryStringParameters']['a'])
+    b = int(event['queryStringParameters']['b'])
+    result = a + b
+    response = {
+        'statusCode': 200,
+        'body': str(result)
+    }
+    return response
